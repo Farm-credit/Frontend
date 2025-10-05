87bdb5f32ed32dc358228027386a915c4663e438
Gilbertttt <umorengilbert17@gmail.com>
2025-10-05 20:34:38 +0100

    feat: Build work section layout and 3-card grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import FAQSection from "@/components/faq-section";
+import HowItWorksSection from "@/components/works-section";
 
 export default function Home() {
   return (
-    <FAQSection/>
+    <>
+      <HowItWorksSection/>
+      <FAQSection/>
+    </>
   );
 }
